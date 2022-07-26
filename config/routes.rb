1c353f5d8ff694e2df00625bc571a12711802f61
Bruno Crivelaro <bruno.crivelaro@toptal.com>
2022-07-26 18:34:37 -0300

    setup root paths and add name to sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'home#index'
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
