de2b26e515140ec9e41dbb3f84c793698e5e9dcf
Bruno Crivelaro <bruno.crivelaro@toptal.com>
2022-07-28 20:22:09 -0300

    add current_account helper method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :current_account
+
+  def current_account
+    @current_account ||= current_user.account
+  end
+
   protected
 
   def configure_permitted_parameters
