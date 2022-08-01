09e7ef7c0b0c5b1ba3c9a1ab48102f8ddf20ae09
Bruno Crivelaro <bruno.crivelaro@toptal.com>
2022-07-31 22:04:04 -0300

    add spec for user sign up failure scenario

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Registrations', type: :request do
-  it 'creates an account for the signed up user' do
-    expect do
-      post '/users',
-           params: { user: { name: 'name', email: 'email@email.com', password: '123456',
-                             password_confirmation: '123456' } }
-    end.to change { User.count }.by(1).and change { Account.count }.by(1)
+  context 'success scenario' do
+    it 'creates an account for the signed up user' do
+      expect do
+        post '/users',
+            params: { user: { name: 'name', email: 'email@email.com', password: '123456',
+                              password_confirmation: '123456' } }
+      end.to change { User.count }.by(1).and change { Account.count }.by(1)
 
-    expect(response).to redirect_to('/')
+      expect(response).to redirect_to('/')
+    end
+  end
+
+  context 'failure scenario' do
+    it 'does not create an account' do
+      expect do
+        post '/users',
+            params: { user: { name: 'name', email: 'invalid', password: '123456',
+                              password_confirmation: '123456' } }
+      end.to_not change { Account.count }
+
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
